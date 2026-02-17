e6fa3e185bf368fb0bfd0ba2af72a703c845b0b1
noita0308 <noita.anpanman@gmail.com>
2026-02-17 16:22:38 +0900

    Add GitHub Actions workflow and update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://noita0308.github.io',
+	base: '/my-astro-blog'
 	integrations: [mdx(), sitemap()],
 });
