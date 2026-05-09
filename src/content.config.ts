74a2899dcc1f37415861669a62909b14555c311b
noita0308 <noita.anpanman@gmail.com>
2026-05-10 01:10:09 +0900

    Reapply "nowとtodayでnotesにおける日付を出力できるように修正。"
    This reverts commit 7fa973c6328fda5e9ed33c5b582a4b1dfeecf13e.

--- src/content.config.ts
+++ src/content.config.ts
@@ -41,10 +41,10 @@ const post = defineCollection({
 const note = defineCollection({
 	loader: glob({ base: "./src/content/note", pattern: "**/*.{md,mdx}" }),
 	schema: baseSchema.extend({
+		// baseSchemaのtitleを「任意（optional）」で上書きする
+        title: titleSchema.optional(),
 		description: z.string().optional(),
-		publishDate: z.iso
-			.datetime({ offset: true }) // Ensures ISO 8601 format with offsets allowed (e.g. "2024-01-01T00:00:00Z" and "2024-01-01T00:00:00+02:00")
-			.transform((val) => new Date(val)),
+		publishDate: z.coerce.date(),
 	}),
 });
 
