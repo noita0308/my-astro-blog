cb8e97bf7b1dab5ae696b7c77d1e65730f74ecb9
noita0308 <noita.anpanman@gmail.com>
2026-05-10 15:40:59 +0900

    サイトのタイトルをultrafast!!!に変更

--- src/site.config.ts
+++ src/site.config.ts
@@ -10,7 +10,7 @@ export const siteConfig: SiteConfig = {
 		- The link value found in src/components/layout/Header.astro L:35
 		- In the footer found in src/components/layout/Footer.astro L:12
 	*/
-	title: "noitablog",
+	title: "ultrafast!!!",
 	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
 	author: "noita",
 	// Used as the default description meta property and webmanifest description
